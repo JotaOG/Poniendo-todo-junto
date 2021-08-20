2bc217720c690d29d498af8d7812c67bd61c66d1
JotaOG <85458044+JotaOG@users.noreply.github.com>
2021-08-20 16:30:52 -0400

    App.css

---
+++ PersonCard.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class PersonCard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
+    plusAge = () => {
+        this.setState({age: this.state.age + 1});
+    }
+    render(){
+        return(
+            <div>
+                <h1>{this.props.lastName}, {this.props.firstName}</h1>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={this.plusAge}>{this.props.text}</button>
+            </div>        
+        );
+    }
+}
+
+export default PersonCard;
